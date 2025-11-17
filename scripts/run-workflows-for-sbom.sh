2cbd666a1a72eb45d711a0911d10bc3455493fb4
Piotr P. Karwasz <piotr@github.copernik.eu>
2025-11-17 13:00:47 +0100

    Add script to generate all call graphs for an SBOM

---
+++ scripts/run-workflows-for-sbom.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SBOM="$1"
+if [ -z "$SBOM" ]; then
+    echo "Usage: $0 <path-to-sbom>"
+    exit 1
+fi
+
+jq -r '.components[].purl' "$SBOM" | while read -r purl; do
+    # Add your processing logic here
+    tmp=${purl#pkg:maven/}
+    tmp=${tmp%\?*}
+    groupId=${tmp%%/*}
+    tmp=${tmp#*/}
+    artifactId=${tmp%%@*}
+    version=${tmp#*@}
+    callgraph_file="metadata/$groupId/$artifactId/$version/callgraph.json"
+    if [ -f "$callgraph_file" ]; then
+        continue
+    fi
+    location_file="metadata/$groupId/$artifactId/location.json"
+    if [ -f "$location_file" ]; then
+        gh workflow run generate-call-graph.yaml -f artifact="$groupId:$artifactId:$version"
+        exit 0
+    else
+        echo "Missing location file for: $groupId:$artifactId:$version" >&2
+    fi
+done
